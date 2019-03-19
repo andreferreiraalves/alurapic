52ccfee250316c78b1e24338f66804896f5dc09b
André Alves Ferreira <andre.ferreira.alves@gmail.com>
2019-03-19 20:38:30 -0300

    new components

--- web/src/app/photos/photo/photo.service.ts
+++ web/src/app/photos/photo/photo.service.ts
@@ -3,12 +3,14 @@ import { Injectable } from '@angular/core';
 
 import { Photo } from './photo';
 
+const API = 'http://localhost:3000';
+
 @Injectable({ providedIn: 'root' })
 export class PhotoService {
 
     constructor(private http: HttpClient) {}
 
     listFromUser(userName: string){
-        return this.http.get<Photo[]>('http://localhost:3000/flavio/photos');
+        return this.http.get<Photo[]>(API + '/' + userName + '/photos');
     }
 }
