d1cecc7cf9502ac66221766cd3b4c1a11ed55910
André Alves Ferreira <andre.ferreira.alves@gmail.com>
2019-03-19 23:13:39 -0300

    Melhorias de layout, usuário

--- web/src/app/photos/photo-list/photos/photos.component.ts
+++ web/src/app/photos/photo-list/photos/photos.component.ts
@@ -8,21 +8,21 @@ import { Photo } from '../../photo/photo';
   styleUrls: ['./photos.component.css']
 })
 export class PhotosComponent implements OnChanges {
-  
+
   @Input() photos: Photo[] = [];
   rows: any[] = [];
-  
+
   constructor() { }
-  
+
   ngOnChanges(changes: SimpleChanges): void {
-    if(changes.photos)
+    if (changes.photos)
       this.rows = this.groupColumns(this.photos);
   }
 
-  groupColumns(photos: Photo[]){
+  groupColumns(photos: Photo[]) {
     const newRows = [];
 
-    for(let index = 0; index < photos.length; index += 3){
+    for (let index = 0; index < photos.length; index += 3) {
       newRows.push(photos.slice(index, index + 3));
     }
 
